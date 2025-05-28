38961ef6afbe5f0ef7cfb17bf44a19a0824e16f6
kolesnikovav <a9104656059@yandex.ru>
2025-05-28 12:13:56 +0300

    исправление координат позиций точек

--- src/common.ts
+++ src/common.ts
@@ -1,18 +1,14 @@
 import type { LngLatBounds, LngLat, Feature } from "ymaps3";
 
-// Function for generating a pseudorandom number
-const seed = (s: number) => () => {
-    const a = Math.sin(s) * 10000;
-    return a - Math.floor(s);
-};
-
-const rnd = seed(10000); // () => Math.random()
-
 // Generating random coordinates of a point [lng, lat] in a given boundary
-const getRandomPointCoordinates = (bounds: LngLatBounds): LngLat => [
-    bounds[0][0] + (bounds[1][0] - bounds[0][0]) * rnd(),
-    bounds[1][1] + (bounds[0][1] - bounds[1][1]) * rnd()
-];
+const getRandomPointCoordinates = (bounds: LngLatBounds): LngLat => {
+  const lngRange = bounds[1][0] - bounds[0][0];
+  const latRange = bounds[1][1] - bounds[0][1];
+  return [
+    bounds[0][0] + Math.random() * lngRange,
+    bounds[1][1] + Math.random() * latRange
+  ]
+};
 
 // A function that creates an array with parameters for each clusterer random point
 export const getRandomPoints = (count: number, bounds: LngLatBounds): Feature[] => {
