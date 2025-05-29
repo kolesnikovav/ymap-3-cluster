05b7d8811cd06e3476a335d20281ef66525c926d
kolesnikovav <a9104656059@yandex.ru>
2025-05-29 12:23:50 +0300

    fix

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:kolesnikovav/ymap-3-cluster.git main:gh-pages
+git push -f git@github.com:kolesnikovav/ymap-3-cluster.git master:gh-pages
 
 cd -
