2ebb17e2e2cd93d1d23d40804da9bbfae0038852
kolesnikovav <a9104656059@yandex.ru>
2025-05-29 12:09:14 +0300

    add deploy

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+set -e
+
+yarn build
+
+cd dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:kolesnikovav/ymap-3-cluster.git main:gh-pages
+
+cd -
