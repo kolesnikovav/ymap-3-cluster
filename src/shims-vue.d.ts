378d01ce6867a539540821a995d9220415175923
kolesnikovav <a9104656059@yandex.ru>
2025-05-29 12:26:03 +0300

    add gh-pages

--- src/shims-vue.d.ts
+++
@@ -1,7 +0,0 @@
-declare module '*.vue' {
-    import type { DefineComponent } from 'vue'
-    // biome-ignore lint/complexity/noBannedTypes: <explanation>
-    // biome-ignore lint/suspicious/noExplicitAny: <explanation>
-        const component: DefineComponent<{}, {}, any>
-    export default component
-}
